651c21516585b2e6c98f67f24818667ce1328041
xf8b <xf8b.inquiries@protonmail.com>
2020-06-27 13:26:43 -0700

    Fixed magic wand not summoning explosions in the correct place

--- src/main/java/io/github/xf8b/testmod/items/MagicWand.java
+++ src/main/java/io/github/xf8b/testmod/items/MagicWand.java
@@ -8,8 +8,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.RayTraceContext;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 
 public class MagicWand extends Item {
@@ -22,8 +21,23 @@ public class MagicWand extends Item {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        BlockRayTraceResult rayTraceResult = worldIn.rayTraceBlocks(new RayTraceContext(playerIn.getLookVec(), playerIn.getPositionVector(), RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.ANY, playerIn));
+        BlockRayTraceResult rayTraceResult = (BlockRayTraceResult) rayTrace(worldIn, playerIn, RayTraceContext.FluidMode.ANY);
         TestModPacketHandler.INSTANCE.sendToServer(new ExplodePacket(playerIn.getPosition(), rayTraceResult));
         return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
+    }
+
+    protected static RayTraceResult rayTrace(World world, PlayerEntity player, RayTraceContext.FluidMode fluidMode) {
+        float f = player.rotationPitch;
+        float f1 = player.rotationYaw;
+        Vec3d vec3d = player.getEyePosition(1.0f);
+        float f2 = MathHelper.cos(-f1 * ((float) Math.PI / 180f) - (float) Math.PI);
+        float f3 = MathHelper.sin(-f1 * ((float) Math.PI / 180f) - (float) Math.PI);
+        float f4 = -MathHelper.cos(-f * ((float) Math.PI / 180f));
+        float f5 = MathHelper.sin(-f * ((float) Math.PI / 180f));
+        float f6 = f3 * f4;
+        float f7 = f2 * f4;
+        double d0 = 100d;
+        Vec3d vec3d1 = vec3d.add((double) f6 *  d0, (double) f5 * d0, (double) f7 * d0);
+        return world.rayTraceBlocks(new RayTraceContext(vec3d, vec3d1, RayTraceContext.BlockMode.COLLIDER, fluidMode, player));
     }
 }
